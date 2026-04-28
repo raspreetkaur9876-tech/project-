3882a0a123acb16a5bf302cdcbbe0eb1bb0eebc2
raspreetkaur9876-tech <raspreetkaur9876@gmail.com>
2026-04-28 17:13:48 +0530

    Update train_model.py

--- train_model.py
+++ train_model.py
@@ -1,20 +1,31 @@
 import pandas as pd
 import pickle
+from sklearn.model_selection import train_test_split
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.naive_bayes import MultinomialNB
 
+# Load dataset
 data = pd.read_csv("dataset.csv")
 
+# dataset should have two columns: label, message
 X = data["message"]
 y = data["label"]
 
-vectorizer = TfidfVectorizer(stop_words="english")
-X_vector = vectorizer.fit_transform(X)
+# Convert text into numerical features
+vectorizer = TfidfVectorizer(stop_words='english')
+X_vectorized = vectorizer.fit_transform(X)
 
+# Split dataset
+X_train, X_test, y_train, y_test = train_test_split(
+    X_vectorized, y, test_size=0.2, random_state=42
+)
+
+# Train model
 model = MultinomialNB()
-model.fit(X_vector, y)
+model.fit(X_train, y_train)
 
+# Save model
 pickle.dump(model, open("spam_model.pkl", "wb"))
 pickle.dump(vectorizer, open("vectorizer.pkl", "wb"))
 
-print("Model trained successfully")
+print("Model trained and saved!")
